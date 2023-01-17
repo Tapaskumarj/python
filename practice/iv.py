96b885d5c434d03aa4e7accbbb986beb53e13c4f
tapas kumar <tapas123@gmail.com>
2023-01-17 09:38:27 +0530

    adding to dicts

--- practice/iv.py
+++ practice/iv.py
@@ -1,5 +1,12 @@
+'''
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 l=zip(keys,values)
 d=dict(l)
 print(d)
+'''
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict1.update(dict2)
+print(dict1)
