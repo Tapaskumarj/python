2cbf4c178db6947ff141b473c742055a48b8fdfb
tapas kumar <tapas123@gmail.com>
2023-01-17 10:22:18 +0530

    nested prog demo

--- practice/func.py
+++ practice/func.py
@@ -10,3 +10,14 @@ def show(a,b=20):
     print(a+b)
 show(20,10)
 '''
+'''
+def add(a,b):
+    def inner():
+       result=a+b
+       return result
+    i=inner()
+    return i+5
+    
+a=add(10,20)
+print(a)
+'''
