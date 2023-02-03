17df0a7777c3abe254bd7ac3613de8fa353ecece
tapas <tapas@gmail.com>
2023-02-03 08:16:26 +0530

    Revert "decor demo"
    This reverts commit d5b7ed62dc53bdbc6822e69dceeaa31231c95ffe.

--- practice/iv.py
+++ practice/iv.py
@@ -1,12 +1,8 @@
-def decor(func):
-    def inner(name):
-        if name == 'tapas':
-            print('hello',name,'good morning.')
-        else:
-            func(name)
-    return inner
-@decor
-def show(name):
-    print('hello',name,'good evening.')
-show('tapas')
-show('ramesh')
+def mygen(n):
+    for i in range(1,n+1):
+        yield i
+a=mygen(5)
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
