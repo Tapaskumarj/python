52dd7b65c9c3c4dec71605add9edb8f1e51c6b03
Tapas kumar <Tapas@gmail.com>
2023-07-01 17:33:28 +0530

    adding a rand.py file

---
+++ practice/rand.py
@@ -0,0 +1,12 @@
+#random() function:
+from random import *
+for i in range(10):
+     print(random())
+#randint() function:
+from random import * 
+for i in range(10):
+     print(randint(1,100))
+#uniform():
+from random import *
+for i in range(10):
+     print(uniform(1,10))
