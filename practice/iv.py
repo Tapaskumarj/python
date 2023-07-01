406c70c04993fde61d3c048642657ef3abea0e44
Tapas kumar <Tapas@gmail.com>
2023-07-01 17:27:32 +0530

    modify in iv.py file

--- practice/iv.py
+++ practice/iv.py
@@ -1,4 +1,19 @@
-a=[1,2,3,4,5]
-from functools import reduce
-l=reduce(lambda x,y:x+y,a)
-print(l)
+class p:
+    def __init__(self):
+        print('this is from parent class const')
+
+    def m(self):
+        print('this is from parent class method')
+
+class c(p):
+    def __init__(self):
+        super().__init__()
+        print('this is from child class const')
+
+    def m(self):
+        super().m()
+        print('this is from child class method')
+
+o=c().m()
+       
+    
