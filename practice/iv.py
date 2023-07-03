82c3f84f8fd0ce8d29414978187523475cf8fef0
Tapas kumar <Tapas@gmail.com>
2023-07-03 08:13:15 +0530

    2 files are modifyed

--- practice/iv.py
+++ practice/iv.py
@@ -1,14 +1,19 @@
-def decor(func):
-    def inner(name):
-        if name =='tapas':
-            print('hello',name,'good morning')
-        else:
-            func(name)
-    return inner
+class p:
+    def __init__(self):
+        print('this is from parent class const')
 
-@decor
-def show(name):
-    print('hello',name,'how are you')
+    def m(self):
+        print('this is from parent class method')
 
-show('tapas')
-show('sumit')
+class c(p):
+    def __init__(self):
+        super().__init__()
+        print('this is from child class const')
+
+    def m(self):
+        super().m()
+        print('this is from child class method')
+
+o=c().m()
+       
+    
